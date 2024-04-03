5fd1c412fa14d8e02d4a0e2850edaedf3e357074
smileluck <12386904+smileluck@users.noreply.github.com>
2024-04-03 13:02:46 +0800

    feat: init

---
+++ smilex-maven/smilex-maven-study/src/test/java/top/zsmile/test/basic/lambda/SFunction.java
@@ -0,0 +1,9 @@
+package top.zsmile.test.basic.lambda;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface SFunction<T, R> extends Function<T, R>, Serializable {
+
+}
---
+++ smilex-maven/smilex-maven-study/src/test/java/top/zsmile/test/jvm/classloader/HelloByteCodePrint.java
@@ -0,0 +1,7 @@
+package top.zsmile.test.jvm.classloader;
+
+public class HelloByteCodePrint {
+    public static void main(String[] args) {
+        System.out.println("Hello ByteCode");
+    }
+}
